e71cdf3402e842197e4c199354c169180d8c1d11
Kirk82 <kirkdaly@gmail.com>
2025-09-18 20:50:43 +1000

    How man legs an animal has

--- src/main.rs
+++ src/main.rs
@@ -3,51 +3,29 @@ use rng::*;
 fn main() {
     let rng_generator = Rng::new();
 
-    let first_value = rng_generator.gen_range(0..2);
-    let second_value = rng_generator.gen_range(0..2);
-    let total = first_value * second_value;
-
-    let total_in_words = match total {
-        0 => TotalInWords::EatPie,
-        1 => TotalInWords::Mmmm,
-        2 => TotalInWords::ILikeDonuts,
-        3 => TotalInWords::Help,
-        4 => TotalInWords::Me,
-        _ => panic!(),
-    };
-
-    #[derive(Debug)]
-    enum TotalInWords {
-        EatPie,
-        Mmmm,
-        ILikeDonuts,
-        Help,
-        Me,
-    }
-
-    println!("value {:?} = {}", total_in_words, total);
-
-    let animal_selector = rng_generator.gen_range(1..2);
+    let animal_selector = rng_generator.gen_range(1..4);
 
     let choosen_animal = match animal_selector {
         1 => Animal::Cat,
         2 => Animal::Human,
+        3 => Animal::Dolphin,
         _ => panic!(),
     };
 
-    let animal_leg_needed: Animal;
+    let animal_legs;
 
-    let animal_legs = match animal_leg_needed {
+    animal_legs = match choosen_animal {
         Animal::Cat => 4,
         Animal::Human => 2,
-        _ => panic!(),
+        Animal::Dolphin => 0,
     };
 
-    println!("{:?} has legs", choosen_animal);
+    println!("A {:?} has {} legs", choosen_animal, animal_legs);
 
     #[derive(Debug)]
     enum Animal {
         Cat,
         Human,
+        Dolphin,
     }
 }
