449df948a309ad13b8010f3dbf104f2413e3f67e
Kirk82 <kirkdaly@gmail.com>
2025-09-19 21:07:01 +1000

    Comments

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,10 @@ use rng::*;
 fn main() {
     let rng_generator = Rng::new();
 
+    //picks a random number
     let animal_selector = rng_generator.gen_range(1..4);
 
+    // Assigns the random number to an animal from the Animal Enum
     let choosen_animal = match animal_selector {
         1 => Animal::Cat,
         2 => Animal::Human,
@@ -14,14 +16,17 @@ fn main() {
         _ => panic!(),
     };
 
+    //initialises the animal_legs varibale
     let animal_legs;
 
+    //Says how many legs an animal has
     animal_legs = match choosen_animal {
         Animal::Cat => 4,
         Animal::Human => 2,
         Animal::Dolphin => 0,
     };
 
+    //This hashmap assigns different habitats to different animals
     let mut habitat_map = HashMap::new();
 
     habitat_map.insert(Animal::Cat, vec!["a house", "the wild"]);
@@ -30,10 +35,13 @@ fn main() {
 
     habitat_map.insert(Animal::Dolphin, vec!["the Ocean", "Sea Wrold"]);
 
+    //connects the animal to the chosen animal variable
     let potential_habitat = habitat_map.get(&choosen_animal).unwrap();
 
+    //randomly chooses a habitat
     let habitat = potential_habitat.sample(rng_generator).unwrap();
 
+    //Prints a line in terimal using randomly generated info
     println!(
         "This {:?} has {} legs and lives in {}",
         choosen_animal, animal_legs, habitat
